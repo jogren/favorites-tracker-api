dc13f981fbdbf16274e014ee0b18b65fc03024cd
Jacob Ogren <jogren0044@gmail.com>
2019-10-17 12:23:22 -0600

    Remove production object from knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,5 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/favorites_tracker',
     useNullAsDefault: true
-  },
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL + `?ssl=true`,
-    useNullAsDefault: true
   }
 };
